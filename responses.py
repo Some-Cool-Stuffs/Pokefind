6b2915d9dd62538897c4c8fb376a6560ed394e95
Suno <ilovesuno@proton.me>
2025-02-11 18:36:04 -0700

    Add coloring changes

--- responses.py
+++ responses.py
@@ -1,8 +1,12 @@
+import colorama as Colors;
+
+# Classes
+
 class Responses:
     def Failure(Message) -> None:
         if Message:
-            print(f"[-] Failed using Response: {Message}")
+            print(Colors.Fore.RED + Colors.Style.BRIGHT + f"Failed using Response: {Colors.Style.RESET_ALL}{Colors.Style.BRIGHT}{Message}{Colors.Style.RESET_ALL}")
 
     def Passed(Message: str) -> None:
         if Message:
-            print(f"[•] {Message}")
+            print(f"{Colors.Style.BRIGHT}{Message}{Colors.Style.RESET_ALL}")
